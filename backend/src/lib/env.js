278e71d3fda6df581cb2bf1cf16cc05bd5600964
Ahmer Altaf <ahmeraltaf3@gmail.com>
2026-09-25 19:41:34 +0500

    DB and tools setuo

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv"
-dotenv.config()
+dotenv.config({quiet:true})
 
 const ENV={
     PORT:process.env.PORT||3000,
-    NODE_ENV:process.env.NODE_ENV||"development"
+    NODE_ENV:process.env.NODE_ENV||"development",
+    MONGO_URI:process.env.MONGO_URI
 }
+
 
 export default ENV
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,21 @@
 import app from "./app.js"
 import ENV from "./lib/env.js"
+import connectDB from "./lib/db.js"
+import dns from "dns"
+dns.setServers(["8.8.8.8"])
 
-app.listen(ENV.PORT,()=>console.log(`server is running on port ${process.env.PORT}`))
+const startServer=async()=>{
+    try {
+        await connectDB()
+        console.log('DB connected successfully');
+
+        app.listen(ENV.PORT,()=>console.log(`server is running on port ${process.env.PORT}`))
+        
+    } catch (error) {
+        console.error('Error starting the server',error);
+    
+        
+    }
+}
+
+startServer()
